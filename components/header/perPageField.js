7314cb27e2f9ecd28c39a7f9853bde8f341eb4bd
NEWHELL69 <kushagra0304@gmail.com>
2024-01-22 03:20:02 +0530

    perPage max limit

--- components/header/perPageField.js
+++ components/header/perPageField.js
@@ -3,6 +3,10 @@ import { showUserRepos } from "../main/userRepos.js";
 
 (() => {
     document.querySelector("#perPageReposBtn").addEventListener("click", () => {
+        if(document.querySelector("#perPageReposField").value > 100) {
+            return;
+        }
+
         setGlobal("perPageRepos", document.querySelector("#perPageReposField").value);
 
         if(getGlobal("username") !== ""){
