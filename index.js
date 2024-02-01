5c587ad2fa295b8139911859e39df75d61d0e6fc
NEWHELL69 <kushagra0304@gmail.com>
2024-02-01 18:27:19 +0530

    added accelerometer

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 import './components/header/userSearch.js'
 import './components/header/perPageField.js'
 import './components/main/searchUserRepos.js'
+
+// Check if Accelerometer is supported
+if ('Accelerometer' in window) {
+    // Request permission to access sensors
+    navigator.permissions.query({ name: 'accelerometer' })
+      .then(permissionStatus => {
+        if (permissionStatus.state === 'granted') {
+          // Accelerometer permission granted
+  
+          // Create an instance of the Accelerometer sensor
+          const accelerometer = new Accelerometer({ frequency: 60 }); // Set desired update frequency
+  
+          // Define an event listener for sensor updates
+          accelerometer.addEventListener('reading', () => {
+            // Access acceleration data from the sensor
+            const acceleration = accelerometer.reading;
+  
+            // Log acceleration values
+            console.log("Acceleration along x-axis: " + acceleration.x);
+            console.log("Acceleration along y-axis: " + acceleration.y);
+            console.log("Acceleration along z-axis: " + acceleration.z);
+          });
+  
+          // Start the sensor
+          accelerometer.start();
+        } else {
+          console.log('Permission to access accelerometer is denied.');
+        }
+      })
+      .catch(error => {
+        console.error('Error while requesting accelerometer permission:', error);
+      });
+  } else {
+    console.log('Accelerometer not supported on this device.');
+  }
+  
--- services/utils/URLs.js
+++ services/utils/URLs.js
@@ -1,7 +1,5 @@
 const baseURL = new URL("https://api.github.com");
 
-export const searchUsersURL = new URL("/search/users", baseURL);
-
 export const getUserURL = (username) => {
     return new URL(`/users/${username}`, baseURL);
 }
@@ -9,5 +7,7 @@ export const getUserURL = (username) => {
 export const getUserReposURL = (username) => { 
     return new URL(`/users/${username}/repos`, baseURL);
 }
+
+export const searchUsersURL = new URL("/search/users", baseURL);
 
 export const searchReposURL = new URL(`/search/repositories`, baseURL);
